83b6ce87915ab7eadb70f882c4e1ef41c4d9e3b0
gauravop40 <gauravop7080@gmail.com>
2024-08-16 08:56:12 +0000

    operation

---
+++ 01-basics/operation.js
@@ -0,0 +1,13 @@
+let value=3;
+let negvalue=-value;
+console.log(negvalue);
+console.log(2+2);
+console.log(2-2);
+console.log(2**2);
+console.log(2%3);
+console.log(2/2);
+let str1=" gaurav"
+let str2="  chaubey"
+
+let str3= str1+str2
+console.log(str3);
